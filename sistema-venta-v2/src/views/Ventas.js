d02a97908a75ec894eff923c6491090fed5d9d77
Gabriela López <denisgabylopez@gmail.com>
2021-10-12 15:28:46 -0500

    Agregar, modificar una venta y listar

--- sistema-venta-v2/src/views/Ventas.js
+++ sistema-venta-v2/src/views/Ventas.js
@@ -2,20 +2,21 @@ import {DataGrid} from '@material-ui/data-grid';
 import React, {useState} from 'react';
 import { Button } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
+import ModalEditar from '../components/EditarVenta/Modal';
 
 const columnas = [
-    { field: "id", headerName: "ID Venta", width: 100 },
-    { field: "idProducto", headerName: "ID Producto", width: 100 },
-    { field: "nombreProducto", headerName: "Producto", width: 300 },
-    { field: "valorUnitario", headerName: "Valor Unitario", width: 200 },
-    { field: "cantidad", headerName: "Cantidad", width: 100 },
+    { field: "id", headerName: "ID Venta", width: 135 },
+    { field: "idProducto", headerName: "ID Producto", width: 160 },
+    { field: "nombreProducto", headerName: "Producto", width: 250 },
+    { field: "valorUnitario", headerName: "Valor Unitario", width: 170 },
+    { field: "cantidad", headerName: "Cantidad", width: 150 },
     { field: "fecha", headerName: "Fecha", width: 200 },
-    { field: "idCliente", headerName: "ID Cliente", width: 100 },
+    { field: "idCliente", headerName: "ID Cliente", width: 160 },
     { field: "nombreCliente", headerName: "Cliente", width: 300 },
     { field: "nombreUsuario", headerName: "Encargado", width: 300 },
 ]
 
-var Venta = function (id, idProducto, nombreProducto, valorUnitario,
+var VentaL = function (id, idProducto, nombreProducto, valorUnitario,
     cantidad, fecha, idCliente, nombreCliente, nombreUsuario) {
     this.id = id;
     this.idProducto = idProducto;
@@ -27,20 +28,58 @@ var Venta = function (id, idProducto, nombreProducto, valorUnitario,
     this.nombreCliente = nombreCliente;
     this.nombreUsuario = nombreUsuario;
 }
+
+var VentaA = function(id, idCliente, fecha, idUsuario, idProducto, cantidad) {
+    this.id = id;
+    this.idProducto = idProducto;
+    this.cantidad = cantidad;
+    this.fecha = fecha;
+    this.idCliente = idCliente;
+    this.idUsuario = idUsuario
+}
+
+const obtenerEstilos = makeStyles(theme => ({
+    botonAgregar: {
+        borderRadius: 15,
+        backgroundColor: "#FFC107",
+        padding: "10px 10px",
+        fontSize: "18px"
+    },
+    botonModificar: {
+        borderRadius: 15,
+        backgroundColor: "#FFC107",
+        padding: "10px 10px",
+        fontSize: "18px"
+    },
+    botonEliminar: {
+        borderRadius: 15,
+        backgroundColor: "#ff5555",
+        borderColor: "#FFC107",
+        padding: "10px 10px",
+        fontSize: "18px"
+    }
+
+}));
+
 const Ventas = () => {
+    const estilos = obtenerEstilos();
 
     const [ventas, setVentas] = useState([]);
 
     const [estadoListado, setEstadoListado] = useState(true);
 
+    const [estadoModal, setEstadoModal] = useState(false);
+
+    const [ventaEditada, setVentaEditada] = useState({})
+
     const obtenerVentas = () => {
         // Consultar la lista de ventas desde la API
-        fetch("http://localhost:3010/ventas", {methos: "get"})
+        fetch("http://localhost:3010/ventas", {method: "get"})
         .then((res) => res.json())
         .then((json) => {
             var ventasT = [];
             json.map((item) => {
-                ventasT.push(new Venta(
+                ventasT.push(new VentaL(
                     item.Id,
                     item.IdProducto,
                     item.NombreProducto,
@@ -61,6 +100,23 @@ const Ventas = () => {
         obtenerVentas();
     }
 
+    const cerrarModal = () => {
+        setEstadoModal(false);
+    }
+
+    const agregar = () => {
+        const ventaE = new VentaA(-1, "","","","","")
+        setVentaEditada(ventaE);
+        setEstadoModal(true);
+    }
+
+    const modificar = () => {
+        const ventaE = ventaSeleccionada;
+        setVentaEditada(ventaE);
+        setEstadoModal(true);
+    }
+    var ventaSeleccionada;
+
     return (
         <div>
             <center>
@@ -69,17 +125,37 @@ const Ventas = () => {
                 </h1>
             </center>
             <div style={{ height: 500, width: '100%' }}>
-                
+            <Button className={estilos.botonAgregar} onClick = {agregar} >
+                    Agregar
+                </Button>
+                <Button className={estilos.botonModificar} onClick = {modificar} >
+                    Modificar
+                </Button>
+                <Button className={estilos.botonEliminar} >
+                    Eliminar
+                </Button>
                 <DataGrid
                     rows={ventas}
                     columns={columnas}
                     pageSize={7}
                     rowsPerPageOptions={[7]}
 
+                    onSelectionModelChange = {(idVentas) => {
+
+                        const ventasSeleccionadas = ventas.filter(
+                            function(fila){
+                                return fila.id == idVentas[0];
+                            }
+                        )
+                        ventaSeleccionada = ventasSeleccionadas[0];
+                    }
+
+                    }
                     
 
                 />
 
+                <ModalEditar open={estadoModal} cerrar={cerrarModal} venta={ventaEditada} />
                
             </div>
         </div>
