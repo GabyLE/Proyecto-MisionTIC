be1778d481e41decf2e86effe4666cf8c0a4c76e
ysaenz07 <90258850+ysaenz07@users.noreply.github.com>
2021-10-13 20:13:55 -0500

    login y logout con gmail

--- sistema-venta-v2/src/index.js
+++ sistema-venta-v2/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Auth0Provider } from "@auth0/auth0-react";
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-p4ficpch.us.auth0.com"
+      clientId="hreE7Eid9qIj6QyoH9ZWVBBXpe7btH9D"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
