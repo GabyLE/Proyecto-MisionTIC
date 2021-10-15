2957d73e8324e362a170fe9458d2d9cd326fb5ea
Gabriela López <denisgabylopez@gmail.com>
2021-10-15 12:22:21 -0500

    recupera archivos

--- apiSistemaVentas/app.js
+++ apiSistemaVentas/app.js
@@ -6,18 +6,12 @@ app.get('/', (req, res) => {
     res.send('Servicio de BD Comercializadora en funcionamiento');
 });
 
-//Cargar librería para 'parseo' de contenido JSON
-// var bodyParser = require('body-parser');
-// app.use(bodyParser.json());
 app.use(express.json());
-//Cargar libreria para habilitar cors
-const cors = require('cors')
-app.use(cors())
+const cors = require('cors');
+app.use(cors());
 
-require("./routes/usuario.rutas")(app);
 require("./routes/venta.rutas")(app);
 
-
 app.listen(puerto, () => {
-    console.log(`Servicio de BD Comercializadora escuchando en http://localhost:${puerto}`);
+    console.log(`Servicio de BD Comercializadora eschuchando en http://localhost:${puerto}`);
 })
